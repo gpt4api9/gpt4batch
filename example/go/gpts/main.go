c9aa66311a63e6306f719c7b2b5c17a0a7bf05c7
gpt4api <gpt4api9@proton.me>
2024-01-13 17:14:32 +0800

    feat: add upload image,file example

--- example/go/gpts/main.go
+++ example/go/gpts/main.go
@@ -16,50 +16,6 @@ limitations under the License.
 
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/google/uuid"
-	"gitlab.com/gpt4batch"
-	"gitlab.com/gpt4batch/client"
-)
-
 func main() {
-	cc := client.NewClient()
 
-	// 上传文件
-	fileResp, err := cc.Upload(context.Background(), &gpt4batch.UploadRequest{
-		Source: &gpt4batch.Source{
-			ID:          uuid.New().String(),
-			UploadURL:   "https://beta.gpt4api.plus/standard/uploaded",
-			Name:        "<随意取一个name>",
-			AccessToken: "<填写控制台access_token>",
-		},
-		UploadPath:     "./example.txt",
-		ConversationId: "",
-		UploadType:     gpt4batch.MyFiles,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("<返回数据：>", fileResp)
-
-	// 上传图片
-	imageResp, err := cc.Upload(context.Background(), &gpt4batch.UploadRequest{
-		Source: &gpt4batch.Source{
-			ID:          uuid.New().String(),
-			UploadURL:   "https://beta.gpt4api.plus/standard/uploaded",
-			Name:        "<随意取一个name>",
-			AccessToken: "<填写控制台access_token>",
-		},
-		UploadPath:     "",
-		ConversationId: "",
-		UploadType:     gpt4batch.Multimodal,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("<返回数据：>", imageResp)
 }
---
+++ example/go/upload/image/main.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2024 The gpt4batch Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"gitlab.com/gpt4batch"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	filename := "./1.jpeg"
+
+	fileBytes, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := resty.New().
+		SetTimeout(8*time.Second).
+		R().
+		EnableTrace().
+		SetAuthToken("<YOUR_ACCESS_TOKEN>").
+		SetHeader("Content-Type", "multipart/form-data").
+		SetFormData(map[string]string{
+			"conversation_id": "",
+			"type":            gpt4batch.Multimodal,
+		}).
+		SetFileReader("file", filepath.Base(filename), bytes.NewReader(fileBytes)).
+		Post("https://beta.gpt4api.plus/standard/uploaded")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		panic(resp.Status())
+	}
+
+	var result gpt4batch.UploadResponse
+	if err = json.Unmarshal(resp.Body(), &result); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("<Upload>: ", result)
+}
